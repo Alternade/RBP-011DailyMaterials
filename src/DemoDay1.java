566874bf38cd2d2f577f11fa721fa7acecb339c5
Arpit Sahu <aptsahu2@gmail.com>
2023-07-07 09:36:01 +0530

    7-7-23: Basics of Java

---
+++ src/DemoDay1.java
@@ -0,0 +1,23 @@
+public class DemoDay1 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        //is the method which returns output
+        System.out.println("Arpit");
+        //Printing my name
+        System.out.println("Gitesh");
+        //there are 2 particular words we must remember
+        //initialization- it is process of creating a variable and assigning a value
+        //declaration-it is a process of creating a variable
+        int a;
+        //created a variable called a
+        int b = 10;
+        a = 15;
+        System.out.println("Value of a = " + a);
+        //process of joining 2 different types
+        System.out.println("Value of b = " + b);
+        //When both are similar datatypes it prefers arithmetic over concatenation
+        System.out.println(a +" hello "+ b);
+
+    }
+}
+
