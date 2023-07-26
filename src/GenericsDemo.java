af4da01b73e2e6a1badcb1c7f60a5269ea0ac3c6
Arpit Sahu <aptsahu2@gmail.com>
2023-07-26 10:43:35 +0530

    Added HashMap

---
+++ src/GenericsDemo.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+
+public class GenericsDemo {
+    public static void main(String[] args) {
+        HashMap< Integer,String > l1 = new HashMap<>();
+        l1.put(1,"Ravinder");
+        l1.put(2,"Gitesh");
+        l1.put(3,"Harshal");
+        l1.put(4,"Ravinder");
+        System.out.println(l1);
+    }
+}
